ef5a8a72997b68d259e5c423060241fbd81eedb2
Chae Young Lee <cylee7133@gmail.com>
2021-02-09 22:25:27 +0900

    default QUAD

--- script.py
+++ script.py
@@ -24,7 +24,7 @@ def default_evaluation_params():
                 'EV_PARAM_IND_CENTER_DIFF_THR': 1,
                 'GT_SAMPLE_NAME_2_ID':'.*([0-9]+).*',
                 'DET_SAMPLE_NAME_2_ID':'.*([0-9]+).*',
-                'LTRB':True, #LTRB:2points(left,top,right,bottom) or 4 points(x1,y1,x2,y2,x3,y3,x4,y4)
+                'LTRB':False, #LTRB:2points(left,top,right,bottom) or 4 points(x1,y1,x2,y2,x3,y3,x4,y4)
                 'CRLF':False, #Lines are delimited by Windows CRLF format
                 'CONFIDENCES':False, #Detections must include confidence value. AP will be calculated
                 'PER_SAMPLE_RESULTS':True, #Generate per sample results and produce data for visualization
